c2a4b8d0a06cd288c2387bc7b0ed9265f4de9f07
pkErbynn <john.erbynn@gmail.com>
2019-11-25 19:10:40 +0000

    done

--- src/main/scala/io/turntabl/scalaOO/MyApp.scala
+++ src/main/scala/io/turntabl/scalaOO/MyApp.scala
@@ -1,12 +1,27 @@
 package io.turntabl.scalaOO
 
+import io.turntabl.grades.Grade
 import io.turntabl.students.TLCStudent
 
 object MyApp extends App {
-    val student1 = new TLCStudent("john", "erbynn", "github.com/pkerbynn", 2015)
-    val student2 = new TLCStudent("anthony", "erbynn")
+  val student1 = new TLCStudent("john", "erbynn", "github.com/pkerbynn", 2015)
+  val student2 = new TLCStudent("anthony", "erbynn", null, 2012)
 
-    println(student1)
-    println(student2)
+  println(student1)
+  println(student2)
 
+  val res = student1.termOfService(2015)
+  println(s"Term of service of ${student1.fname}: $res")
+
+  val res2 = student1.sameTermOfService_?(student2, 2014)
+  println(res2)
+
+  private val str: String = TLCStudent.say()
+  println(str)
+
+  val topGrade: Grade = student1.topGrade()
+  println(s"TopGrad: $topGrade")
+  private val subject: String = student1.topGrade().subject
+  private val grad: Int = student1.topGrade().grad
+  println(s"Grad => Stubject : $grad => $subject")
 }
--- src/main/scala/io/turntabl/students/TLCStudent.scala
+++ src/main/scala/io/turntabl/students/TLCStudent.scala
@@ -5,25 +5,27 @@ import java.time.Year
 class TLCStudent(val fname: String,
                  val lname: String,
                  val githubURL: String = "https://github.com/turntabl",
-                 year: Int = Year.now.getValue   // constructor param and not field
+//                 year: Int = Year.now.getValue   // constructor param and not instance field...
+                 val year: Int = Year.now.getValue   // constructor param and not field
                 ){
   override def toString: String = s"$fname $lname"
 
   val thisYear: Int = Year.now.getValue toInt
   def termOfService(year: Int) =  thisYear - year
 
-  def sameTermOfService_?(student: TLCStudent, year: Int) = student.termOfService(year) equals termOfService(year)   // stu.thisYear.equal(thisYear)
+  def sameTermOfService_?(student: TLCStudent, year: Int) = student.termOfService(student.year) equals termOfService(year)   // stu.thisYear.equal(thisYear)
 
   import io.turntabl.grades._
-  def topGrade() = Grade.apply("Java OO", 99)
-
+  def topGrade() = Grade("Java OO", 99)
 
 }
 
 // factory method (ie. apply()) with companion object
+// i think, static members goes in here
 object TLCStudent{    // object + same class name.....hence, companion
   def apply(fname: String, lname: String,
             githubURL: String): TLCStudent = new TLCStudent(fname, lname, githubURL)  // factory(), hence no `new` keyword in construction
+  def say() =  "static...."
 }
 
 
