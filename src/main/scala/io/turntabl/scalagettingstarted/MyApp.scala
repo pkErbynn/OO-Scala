cca645960a4c4b4abae30fb35d1bda5c0a1ca74d
pkErbynn <john.erbynn@gmail.com>
2019-11-25 11:12:18 +0000

    fxns

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -2,4 +2,21 @@ package io.turntabl.scalagettingstarted
 
 object MyApp extends App {
     print("Hello, Pkay")
+
+    val fname = "John"
+    val town = "Elmina"
+    print(s"$fname was born in $town")
+
+    // can try in scratch pad
+    def threeTimes(name: String) = name * 3     // 3 * name...doesnt work
+    threeTimes("Erbynn")
+
+    def even_?(num: Int) = num % 2 == 0
+    even_?(3)
+
+    def odd_?(num: Int)= !even_?(num)
+
+    def wordsToLowerCase(words: String*) = words map (_.toLowerCase())
+//    def wordsToLowerCase(words: String*) = words.map(_.toLowerCase())        // similar to java
+    wordsToLowerCase("HEY", "JOHN")
 }
